84a27dde8dbd4fbfbf6dc4608b89816d70d80265
mearg19 <magistudy1@gmail.com>
2024-12-08 23:08:14 -0800

    main method and introduction

---
+++ my first one.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello evry one welcome to my repositry");
+    }
+    
+    // Your custom method
+    public static void Mearg() {
+        System.out.println("write what ever you want");
+    }
+}
